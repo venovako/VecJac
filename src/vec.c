075189b5d87130ab4d61370bb457503d0c2020e1
Vedran Novakovic <venovako@venovako.eu>
2021-01-04 00:27:34 +0100

    Add vec.

---
+++ src/vec.c
@@ -0,0 +1,153 @@
+#include "vec.h"
+
+int VSprintf(FILE f[static 1], const char *const h, const VS v)
+{
+  alignas(VA) float s[VSL];
+
+  int ret = (h ? fprintf(f, "\nL: %s\n", h) : 0);
+  if (ret < 0) {
+    perror("fprintf");
+    return -1;
+  }
+
+  if (fflush(f)) {
+    perror("fflush");
+    return -2;
+  }
+
+  VSI(store)(s, v);
+
+  char a[17];
+  for (unsigned i = 0u; i < VSL; ++i) {
+    char *const p = stoa(a, s[i]);
+    if (!p) {
+      perror("sprintf");
+      return -(int)(i + 4u);
+    }
+    if (20 != fprintf(f, "%X: %s\n", i, p)) {
+      perror("fprintf");
+      return -(int)(i + 4u);
+    }
+    ret += 20;
+  }
+
+  return (fflush(f) ? -3 : ret);
+}
+
+int VDprintf(FILE f[static 1], const char *const h, const VD v)
+{
+  alignas(VA) double d[VDL];
+
+  int ret = (h ? fprintf(f, "\nL: %s\n", h) : 0);
+  if (ret < 0) {
+    perror("fprintf");
+    return -1;
+  }
+
+  if (fflush(f)) {
+    perror("fflush");
+    return -2;
+  }
+
+  VDI(store)(d, v);
+
+  char a[26];
+  for (unsigned i = 0u; i < VDL; ++i) {
+    char *const p = dtoa(a, d[i]);
+    if (!p) {
+      perror("sprintf");
+      return -(int)(i + 4u);
+    }
+    if (29 != fprintf(f, "%u: %s\n", i, p)) {
+      perror("fprintf");
+      return -(int)(i + 4u);
+    }
+    ret += 29;
+  }
+
+  return (fflush(f) ? -3 : ret);
+}
+
+int MSprintf(FILE f[static 1], const char *const h, const MS m)
+{
+  if (fflush(f)) {
+    perror("fflush");
+    return -1;
+  }
+
+  int ret = (h ? fprintf(f, "\n%s: ", h) : 0);
+  if (ret < 0) {
+    perror("fprintf");
+    return -2;
+  }
+
+  const unsigned u = MS2U(m);
+  for (unsigned i = 0u, o = (1u << VSL_1); i < VSL; ++i) {
+    if (1 != fprintf(f, "%c", ((u & o) ? '1' : '0'))) {
+      perror("fprintf");
+      return -(int)(i + 4u);
+    }
+    o >>= 1u;
+    ++ret;
+  }
+
+  if (8 != fprintf(f, " (%04X)\n", u)) {
+    perror("fprintf");
+    return -(int)(VSL + 4u);
+  }
+  ret += 8;
+
+  return (fflush(f) ? -3 : ret);
+}
+
+int MDprintf(FILE f[static 1], const char *const h, const MD m)
+{
+  if (fflush(f)) {
+    perror("fflush");
+    return -1;
+  }
+
+  int ret = (h ? fprintf(f, "\n%s: ", h) : 0);
+  if (ret < 0) {
+    perror("fprintf");
+    return -2;
+  }
+
+  const unsigned u = MD2U(m);
+  for (unsigned i = 0u, o = (1u << VDL_1); i < VDL; ++i) {
+    if (1 != fprintf(f, "%c", ((u & o) ? '1' : '0'))) {
+      perror("fprintf");
+      return -(int)(i + 4u);
+    }
+    o >>= 1u;
+    ++ret;
+  }
+
+  if (6 != fprintf(f, " (%02X)\n", u)) {
+    perror("fprintf");
+    return -(int)(VDL + 4u);
+  }
+  ret += 6;
+
+  return (fflush(f) ? -3 : ret);
+}
+
+size_t VSread(float s[static VSL], const size_t V, FILE f[static 1])
+{
+  return fread(s, sizeof(float) * VSL, V, f);
+}
+
+size_t VDread(double d[static VDL], const size_t V, FILE f[static 1])
+{
+  return fread(d, sizeof(double) * VDL, V, f);
+}
+
+size_t VSwrite(const float s[static VSL], const size_t V, FILE f[static 1])
+{
+  return fwrite(s, sizeof(float) * VSL, V, f);
+}
+
+size_t VDwrite(const double d[static VDL], const size_t V, FILE f[static 1])
+{
+  return fwrite(d, sizeof(double) * VDL, V, f);
+}
---
+++ src/vec.h
@@ -0,0 +1,213 @@
+#ifndef VEC_H
+#define VEC_H
+
+#include "common.h"
+
+/*** redefine if changing the vector type ***/
+
+#ifdef __AVX512F__
+#include <immintrin.h>
+#else /* !__AVX512F__ */
+#error AVX-512 instructions not available
+#endif /* ?__AVX512F__ */
+
+/* vector type containing floats */
+#ifdef VS
+#error VS already defined
+#else /* !VS */
+#define VS __m512
+#endif /* ?VS */
+
+/* vector type containing doubles */
+#ifdef VD
+#error VD already defined
+#else /* !VD */
+#define VD __m512d
+#endif /* ?VD */
+
+/* vector length in 32-bit lanes */
+#ifdef VSL
+#error VSL already defined
+#else /* !VSL */
+#define VSL 16u
+#endif /* ?VSL */
+
+/* VSL - 1 */
+#ifdef VSL_1
+#error VSL_1 already defined
+#else /* !VSL_1 */
+#define VSL_1 15u
+#endif /* ?VSL_1 */
+
+/* lg(VSL) */
+#ifdef VSLlg
+#error VSLlg already defined
+#else /* !VSLlg */
+#define VSLlg 4u
+#endif /* ?VSLlg */
+
+/* vector length in 64-bit lanes */
+#ifdef VDL
+#error VDL already defined
+#else /* !VDL */
+#define VDL 8u
+#endif /* ?VDL */
+
+/* VDL - 1 */
+#ifdef VDL_1
+#error VDL_1 already defined
+#else /* !VDL_1 */
+#define VDL_1 7u
+#endif /* ?VDL_1 */
+
+/* lg(VDL) */
+#ifdef VDLlg
+#error VDLlg already defined
+#else /* !VDLlg */
+#define VDLlg 3u
+#endif /* ?VDLlg */
+
+/* vector alignment in bytes */
+#ifdef VA
+#error VA already defined
+#else /* !VA */
+#define VA 64u
+#endif /* ?VA */
+
+/* mask type for float lanes */
+#ifdef MS
+#error MS already defined
+#else /* !MS */
+#define MS __mmask16
+#endif /* ?MS */
+
+/* mask type for double lanes */
+#ifdef MD
+#error MD already defined
+#else /* !MD */
+#define MD __mmask8
+#endif /* ?MD */
+
+/* VS instruction name */
+#ifdef VSI
+#error VSI already defined
+#else /* !VSI */
+#define VSI(x) _mm512_##x##_ps
+#endif /* ?VSI */
+
+/* VD instruction name */
+#ifdef VDI
+#error VDI already defined
+#else /* !VDI */
+#define VDI(x) _mm512_##x##_pd
+#endif /* ?VDI */
+
+/* float <-comparison */
+#ifdef VSLT
+#error VSLT already defined
+#else /* !VSLT */
+#define VSLT _mm512_cmplt_ps_mask
+#endif /* ?VSLT */
+
+/* double <-comparison */
+#ifdef VDLT
+#error VDLT already defined
+#else /* !VDLT */
+#define VDLT _mm512_cmplt_pd_mask
+#endif /* ?VDLT */
+
+#ifdef VSAND
+#error VSAND already defined
+#endif /* VSAND */
+#ifdef VDAND
+#error VDAND already defined
+#endif /* VDAND */
+
+#ifdef VSANDNOT
+#error VSANDNOT already defined
+#endif /* VSANDNOT */
+#ifdef VDANDNOT
+#error VDANDNOT already defined
+#endif /* VDANDNOT */
+
+#ifdef VSOR
+#error VSOR already defined
+#endif /* VSOR */
+#ifdef VDOR
+#error VDOR already defined
+#endif /* VDOR */
+
+#ifdef VSXOR
+#error VSXOR already defined
+#endif /* VSXOR */
+#ifdef VDXOR
+#error VDXOR already defined
+#endif /* VDXOR */
+
+#ifdef MS2U
+#error MS2U already defined
+#endif /* MS2U */
+#ifdef MD2U
+#error MD2U already defined
+#endif /* MD2U */
+
+#ifdef __AVX512DQ__
+#define VSAND(x,y) VSI(and)((x), (y))
+#define VSANDNOT(x,y) VSI(andnot)((x), (y))
+#define VSOR(x,y) VSI(or)((x), (y))
+#define VSXOR(x,y) VSI(xor)((x), (y))
+
+#define VDAND(x,y) VDI(and)((x), (y))
+#define VDANDNOT(x,y) VDI(andnot)((x), (y))
+#define VDOR(x,y) VDI(or)((x), (y))
+#define VDXOR(x,y) VDI(xor)((x), (y))
+#define MD2U(m) _cvtmask8_u32(m)
+#else /* AVX512F only */
+#define VSAND(x,y) _mm512_castsi512_ps(_mm512_and_epi32(_mm512_castps_si512(x), _mm512_castps_si512(y)))
+#define VSANDNOT(x,y) _mm512_castsi512_ps(_mm512_andnot_epi32(_mm512_castps_si512(x), _mm512_castps_si512(y)))
+#define VSOR(x,y) _mm512_castsi512_ps(_mm512_or_epi32(_mm512_castps_si512(x), _mm512_castps_si512(y)))
+#define VSXOR(x,y) _mm512_castsi512_ps(_mm512_xor_epi32(_mm512_castps_si512(x), _mm512_castps_si512(y)))
+
+#define VDAND(x,y) _mm512_castsi512_pd(_mm512_and_epi64(_mm512_castpd_si512(x), _mm512_castpd_si512(y)))
+#define VDANDNOT(x,y) _mm512_castsi512_pd(_mm512_andnot_epi64(_mm512_castpd_si512(x), _mm512_castpd_si512(y)))
+#define VDOR(x,y) _mm512_castsi512_pd(_mm512_or_epi64(_mm512_castpd_si512(x), _mm512_castpd_si512(y)))
+#define VDXOR(x,y) _mm512_castsi512_pd(_mm512_xor_epi64(_mm512_castpd_si512(x), _mm512_castpd_si512(y)))
+#define MD2U(m) _cvtmask16_u32((__mmask16)(m))
+#endif /* __AVX512DQ__ */
+#define MS2U(m) _cvtmask16_u32(m)
+
+/*** end of vector definitions ***/
+
+static inline size_t n2VS(const size_t n)
+{
+  return ((n + VSL_1) >> VSLlg);
+}
+
+static inline size_t n2VD(const size_t n)
+{
+  return ((n + VDL_1) >> VDLlg);
+}
+
+static inline size_t n2NS(const size_t n)
+{
+  return (n2VS(n) << VSLlg);
+}
+
+static inline size_t n2ND(const size_t n)
+{
+  return (n2VD(n) << VDLlg);
+}
+
+extern int VSprintf(FILE f[static 1], const char *const h, const VS v);
+extern int VDprintf(FILE f[static 1], const char *const h, const VD v);
+
+extern int MSprintf(FILE f[static 1], const char *const h, const MS m);
+extern int MDprintf(FILE f[static 1], const char *const h, const MD m);
+
+extern size_t VSread(float s[static VSL], const size_t V, FILE f[static 1]);
+extern size_t VDread(double d[static VDL], const size_t V, FILE f[static 1]);
+
+extern size_t VSwrite(const float s[static VSL], const size_t V, FILE f[static 1]);
+extern size_t VDwrite(const double d[static VDL], const size_t V, FILE f[static 1]);
+
+#endif /* !VEC_H */
