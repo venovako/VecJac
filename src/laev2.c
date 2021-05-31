8a531ee5b1d39ec6b62cf6e8190b724bcc8699b1
Vedran Novakovic <venovako@venovako.eu>
2021-05-31 21:01:31 +0200

    Update laev2.

--- src/laev2.c
+++ src/laev2.c
@@ -1,1 +1,14 @@
 #include "laev2.h"
+
+void dlevd2(const double a11, const double a22, const double a21, double l1[static restrict 1], double l2[static restrict 1], double cs1[static restrict 1], double sn1[static restrict 1])
+{
+  LAPACK_D(laev2)(&a11, &a21, &a22, l1, l2, cs1, sn1);
+}
+
+void zlevd2(const double a11, const double a22, const double a21r, const double a21i, double l1[static restrict 1], double l2[static restrict 1], double cs1[static restrict 1], double _Complex sn1[static restrict 1])
+{
+  const double _Complex a = CMPLX(a11, W_ZERO);
+  const double _Complex b = CMPLX(a21r, -a21i);
+  const double _Complex c = CMPLX(a22, W_ZERO);
+  LAPACK_Z(laev2)(&a, &b, &c, l1, l2, cs1, sn1);
+}
