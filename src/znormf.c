72d44e6cc2cf3bb1ede4c9f7255a7498b2dbbf08
Vedran Novakovic <venovako@venovako.eu>
2021-10-28 19:21:59 +0200

    |z|=hypot(|Re(z)|,|Im(z)|).

--- src/znormf.c
+++ src/znormf.c
@@ -4,13 +4,17 @@
 
 double znormf_(const fnat m[static restrict 1], const double zr[static restrict 1], const double zi[static restrict 1], double e0[static restrict 1], double f0[static restrict 1], double e1[static restrict 1], double f1[static restrict 1])
 {
-  long double sq = 0.0L;
+  long double sqr = 0.0L;
   for (fnat i = 0u; i < *m; ++i) {
     const long double lr = zr[i];
-    sq += (lr * lr);
-    const long double li = zi[i];
-    sq += (li * li);
+    sqr += (lr * lr);
   }
+  long double sqi = 0.0L;
+  for (fnat i = 0u; i < *m; ++i) {
+    const long double li = zi[i];
+    sqi += (li * li);
+  }
+  long double sq = hypotl(sqr, sqi);
   ldbl2ef(sq, e1, f1);
   sq = sqrtl(sq);
   ldbl2ef(sq, e0, f0);
