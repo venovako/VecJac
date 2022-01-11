dfcd3edc8bc5f2c861e91fc5e6c2f9bc4826c962
Vedran Novakovic <venovako@venovako.eu>
2022-01-11 21:15:57 +0100

    Squash a few bugs.

--- src/zvjsvd.c
+++ src/zvjsvd.c
@@ -15,7 +15,7 @@
 #ifdef DBL_MAX_ROT_EXP
 #error DBL_MAX_ROT_EXP already defined
 #else /* !DBL_MAX_ROT_EXP */
-#define DBL_MAX_ROT_EXP 1020
+#define DBL_MAX_ROT_EXP 1021
 #endif /* ?DBL_MAX_ROT_EXP */
 
 fint zvjsvd_(const fnat m[static restrict 1], const fnat n[static restrict 1], double Gr[static restrict VDL], const fnat ldGr[static restrict 1], double Gi[static restrict VDL], const fnat ldGi[static restrict 1], double Vr[static restrict VDL], const fnat ldVr[static restrict 1], double Vi[static restrict VDL], const fnat ldVi[static restrict 1], double eS[static restrict 1], double fS[static restrict 1], const unsigned js[static restrict 1], const unsigned stp[static restrict 1], const unsigned swp[static restrict 1], double work[static restrict VDL], unsigned iwork[static restrict 1])
