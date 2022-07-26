1f952064981f55e89b5e7f35f39ea8021e2ad076
Vedran Novakovic <venovako@venovako.eu>
2022-07-26 20:35:30 +0200

    Add subhypf.

---
+++ etc/subhypf.c
@@ -0,0 +1,19 @@
+#ifdef __ICC
+#include <mathimf.h>
+#else /* !__ICC */
+#include <math.h>
+#endif /* ?__ICC */
+#include <float.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  size_t c = 0u;
+  for (float f = FLT_TRUE_MIN; f < FLT_MIN; f = nextafterf(f, FLT_MIN)) {
+    if (hypotf(f, f) == f)
+      ++c;
+  }
+  (void)fprintf(stdout, "float = %zu\n", c);
+  (void)fflush(stdout);
+  return EXIT_SUCCESS;
+}
