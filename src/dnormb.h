cd6b4ff6eb2a027537f267673fa4dd15370cd558
Vedran Novakovic <venovako@venovako.eu>
2022-08-05 23:46:47 +0200

    small updates

--- src/dnormb.h
+++ src/dnormb.h
@@ -1,8 +1,8 @@
-#ifndef DNORM2_H
-#define DNORM2_H
+#ifndef DNORMB_H
+#define DNORMB_H
 
 #include "common.h"
 
 extern double dnormb_(const fint n[static 1], const double x[static 1], const fint incx[static 1]);
 
-#endif /* !DNORM2_H */
+#endif /* !DNORMB_H */
