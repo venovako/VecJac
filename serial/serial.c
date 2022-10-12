c8e397e6ebe7bf6f0fd6d159e675a9eb33e0029a
Vedran Novakovic <venovako@venovako.eu>
2022-10-12 19:56:10 +0200

    Add the long double routines.

--- serial/serial.c
+++ serial/serial.c
@@ -41,6 +41,18 @@
 #define DBL_SQRT_HUGE 1.34078079299425956E+154
 #endif /* ?DBL_SQRT_HUGE */
 
+#ifdef LDBL_BIG_EXP
+#error LDBL_BIG_EXP already defined
+#else /* LDBL_MAX_EXP - 3 */
+#define LDBL_BIG_EXP 16381
+#endif /* ?LDBL_BIG_EXP */
+
+#ifdef LDBL_SQRT_HUGE
+#error LDBL_SQRT_HUGE already defined
+#else /* sqrtl(LDBL_MAX) */
+#define LDBL_SQRT_HUGE 1.090748135619415929404E+2466
+#endif /* ?LDBL_SQRT_HUGE */
+
 /* in the routines below, -es could be returned instead of es */
 
 int csjac2(const float a11, const float a22, const float a21r, const float a21i, float *const cs, float *const snr, float *const sni, float *const l1, float *const l2)
@@ -151,6 +163,60 @@ int zsjac2(const double a11, const double a22, const double a21r, const double a
   return er;
 }
 
+int wsjac2(const long double a11, const long double a22, const long double a21r, const long double a21i, long double *const cs, long double *const snr, long double *const sni, long double *const l1, long double *const l2)
+{
+#ifndef NDEBUG
+  if (!isfinite(a11))
+    return INT_MIN;
+  if (!isfinite(a22))
+    return INT_MIN;
+  if (!isfinite(a21r))
+    return INT_MIN;
+  if (!isfinite(a21i))
+    return INT_MIN;
+#endif /* !NDEBUG */
+  int e1, e2, er, ei;
+  (void)frexpl(fmaxl(fabsl(a11), LDBL_TRUE_MIN), &e1); e1 = LDBL_BIG_EXP - e1;
+  (void)frexpl(fmaxl(fabsl(a22), LDBL_TRUE_MIN), &e2); e2 = LDBL_BIG_EXP - e2;
+  (void)frexpl(fmaxl(fabsl(a21r), LDBL_TRUE_MIN), &er); er = LDBL_BIG_EXP - er;
+  (void)frexpl(fmaxl(fabsl(a21i), LDBL_TRUE_MIN), &ei); ei = LDBL_BIG_EXP - ei;
+  e1 = ((e1 <= e2) ? e1 : e2);
+  e2 = ((er <= ei) ? er : ei);
+  int es = ((e1 <= e2) ? e1 : e2);
+  const long double
+    a1 = scalbnl(a11, es),
+    a2 = scalbnl(a22, es),
+    ar = scalbnl(a21r, es),
+    ai = scalbnl(a21i, es);
+  long double
+    ar_ = fabsl(ar),
+    ai_ = fabsl(ai);
+  const long double
+    am = fminl(ar_, ai_),
+    aM = fmaxl(ar_, ai_);
+  long double aa = fmaxl(am / aM, 0.0L);
+  aa = sqrtl(fmal(aa, aa, 1.0L)) * aM;
+  ar_ = copysignl(fminl(ar_ / aa, 1.0L), ar);
+  ai_ = ai / fmaxl(aa, LDBL_TRUE_MIN);
+  const long double
+    an = (aa * 2.0L),
+    ad = (a1 - a2),
+    t2 = copysignl(fminl(fmaxl(an / fabsl(ad), 0.0L), LDBL_SQRT_HUGE), ad),
+    t1 = (t2 / (1.0L + sqrtl(fmal(t2, t2, 1.0L)))),
+    s2 = fmal(t1, t1, 1.0L),
+    s1 = sqrtl(s2);
+  *cs = 1.0L / s1;
+  *snr = (ar_ * t1) / s1;
+  *sni = (ai_ * t1) / s1;
+  *l1 = fmal(t1, fmal(a2, t1,  an), a1) / s2;
+  *l2 = fmal(t1, fmal(a1, t1, -an), a2) / s2;
+  er = ((es << 1) | (*l1 < *l2));
+  es = -es;
+  *l1 = scalbnl(*l1, es);
+  *l2 = scalbnl(*l2, es);
+  return er;
+}
+
 int ssjac2(const float a11, const float a22, const float a21, float *const cs, float *const sn, float *const l1, float *const l2)
 {
 #ifndef NDEBUG
@@ -226,5 +292,44 @@ int dsjac2(const double a11, const double a22, const double a21, double *const c
   es = -es;
   *l1 = scalbn(*l1, es);
   *l2 = scalbn(*l2, es);
+  return er;
+}
+
+int xsjac2(const long double a11, const long double a22, const long double a21, long double *const cs, long double *const sn, long double *const l1, long double *const l2)
+{
+#ifndef NDEBUG
+  if (!isfinite(a11))
+    return INT_MIN;
+  if (!isfinite(a22))
+    return INT_MIN;
+  if (!isfinite(a21))
+    return INT_MIN;
+#endif /* !NDEBUG */
+  int e1, e2, er;
+  (void)frexpl(fmaxl(fabsl(a11), LDBL_TRUE_MIN), &e1); e1 = LDBL_BIG_EXP - e1;
+  (void)frexpl(fmaxl(fabsl(a22), LDBL_TRUE_MIN), &e2); e2 = LDBL_BIG_EXP - e2;
+  (void)frexpl(fmaxl(fabsl(a21), LDBL_TRUE_MIN), &er); er = LDBL_BIG_EXP - er;
+  e1 = ((e1 <= e2) ? e1 : e2);
+  int es = ((e1 <= er) ? e1 : er);
+  const long double
+    a1 = scalbnl(a11, es),
+    a2 = scalbnl(a22, es),
+    ar = scalbnl(a21, es),
+    aa = fabsl(ar),
+    as = copysignl(1.0L, ar),
+    an = (aa * 2.0L),
+    ad = (a1 - a2),
+    t2 = copysignl(fminl(fmaxl(an / fabsl(ad), 0.0L), LDBL_SQRT_HUGE), ad),
+    t1 = (t2 / (1.0L + sqrtl(fmal(t2, t2, 1.0L)))),
+    s2 = fmal(t1, t1, 1.0L),
+    s1 = sqrtl(s2);
+  *cs = 1.0L / s1;
+  *sn = (t1 * as) / s1;
+  *l1 = fmal(t1, fmal(a2, t1,  an), a1) / s2;
+  *l2 = fmal(t1, fmal(a1, t1, -an), a2) / s2;
+  er = ((es << 1) | (*l1 < *l2));
+  es = -es;
+  *l1 = scalbnl(*l1, es);
+  *l2 = scalbnl(*l2, es);
   return er;
 }
--- serial/serial.h
+++ serial/serial.h
@@ -17,6 +17,12 @@ extern
 #ifdef __cplusplus
 "C"
 #endif /* __cplusplus */
+int wsjac2(const long double a11, const long double a22, const long double a21r, const long double a21i, long double *const cs, long double *const snr, long double *const sni, long double *const l1, long double *const l2);
+
+extern
+#ifdef __cplusplus
+"C"
+#endif /* __cplusplus */
 int ssjac2(const float a11, const float a22, const float a21, float *const cs, float *const sn, float *const l1, float *const l2);
 
 extern
@@ -24,5 +30,11 @@ extern
 "C"
 #endif /* __cplusplus */
 int dsjac2(const double a11, const double a22, const double a21, double *const cs, double *const sn, double *const l1, double *const l2);
+
+extern
+#ifdef __cplusplus
+"C"
+#endif /* __cplusplus */
+int xsjac2(const long double a11, const long double a22, const long double a21, long double *const cs, long double *const sn, long double *const l1, long double *const l2);
 
 #endif /* !SERIAL_H */
