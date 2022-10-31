bbebfe4f770e9b969928ac3758b372a0c79f8905
Vedran Novakovic <venovako@venovako.eu>
2022-11-01 00:57:34 +0100

    prepare for further updates

--- src/dvjsvd.h
+++ src/dvjsvd.h
@@ -3,7 +3,7 @@
 
 #include "vec.h"
 
-// work: 4 * n elems
+// work: 6 * n elems
 // iwork: n / VDL elems
 extern fint dvjsvd_(const fnat m[static restrict 1], const fnat n[static restrict 1], double G[static restrict VDL], const fnat ldG[static restrict 1], double V[static restrict VDL], const fnat ldV[static restrict 1], double eS[static restrict 1], double fS[static restrict 1], const unsigned js[static restrict 1], const unsigned stp[static restrict 1], const unsigned swp[static restrict 1], double work[static restrict VDL], unsigned iwork[static restrict 1]);
 
--- src/svjsvd.h
+++ src/svjsvd.h
@@ -3,7 +3,7 @@
 
 #include "vec.h"
 
-// work: 4 * n elems
+// work: 6 * n elems
 // iwork: n / VSL elems
 extern fint svjsvd_(const fnat m[static restrict 1], const fnat n[static restrict 1], float G[static restrict VSL], const fnat ldG[static restrict 1], float V[static restrict VSL], const fnat ldV[static restrict 1], float eS[static restrict 1], float fS[static restrict 1], const unsigned js[static restrict 1], const unsigned stp[static restrict 1], const unsigned swp[static restrict 1], float work[static restrict VSL], unsigned iwork[static restrict 1]);
 
