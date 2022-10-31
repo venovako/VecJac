67a325ca0031fdc99ec0daa1b6133e921a2506dd
Vedran Novakovic <venovako@venovako.eu>
2022-10-31 22:32:37 +0100

    prepare for further updates

--- src/zvjsvd.h
+++ src/zvjsvd.h
@@ -3,7 +3,7 @@
 
 #include "vec.h"
 
-// work: 5 * n elems
+// work: 7 * n elems
 // iwork: n / VDL elems
 extern fint zvjsvd_(const fnat m[static restrict 1], const fnat n[static restrict 1], double Gr[static restrict VDL], const fnat ldGr[static restrict 1], double Gi[static restrict VDL], const fnat ldGi[static restrict 1], double Vr[static restrict VDL], const fnat ldVr[static restrict 1], double Vi[static restrict VDL], const fnat ldVi[static restrict 1], double eS[static restrict 1], double fS[static restrict 1], const unsigned js[static restrict 1], const unsigned stp[static restrict 1], const unsigned swp[static restrict 1], double work[static restrict VDL], unsigned iwork[static restrict 1]);
 
