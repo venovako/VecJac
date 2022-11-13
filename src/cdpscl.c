4612a884b01bc7365d06e78260a1e002c1b3ee1a
Vedran Novakovic <venovako@venovako.eu>
2022-11-13 21:55:51 +0100

    Add the compensated summation to the complex scaled dot-product routines.

--- src/cdpscl.c
+++ src/cdpscl.c
@@ -1,5 +1,31 @@
 #include "cdpscl.h"
 
+#ifdef USE_2SUM
+#include "s2sum.h"
+#include "vecdef.h"
+#ifdef INMULSUM
+#error INMULSUM already defined
+#else /* !INMULSUM */
+#define INMULSUM(x,y,s,t,s_,t_)       \
+  xy = _mm512_mul_round_ps(x, y, rni);\
+  a = s;                              \
+  xy_ = _mm512_fmsub_ps(x, y, xy);    \
+  b = _mm512_add_ps(xy, t);           \
+  TwoSum(a,b,a_,b_,s,t);              \
+  a = s_;                             \
+  b = _mm512_add_ps(xy_, t_);         \
+  TwoSum(a,b,a_,b_,s_,t_)
+#endif /* ?INMULSUM */
+#ifdef OUTSUM
+#error OUTSUM already defined
+#else /* !OUTSUM */
+#define OUTSUM(x,y)    \
+  a = x;               \
+  b = y;               \
+  TwoSum(a,b,a_,b_,x,y)
+#endif /* ?OUTSUM */
+#endif /* USE_2SUM */
+
 float complex cdpscl_(const fnat m[static restrict 1], const float xr[static restrict VSL], const float xi[static restrict VSL], const float yr[static restrict VSL], const float yi[static restrict VSL], const float e[static restrict 2], const float f[static restrict 2])
 {
 #ifndef NDEBUG
@@ -35,10 +61,45 @@ float complex cdpscl_(const fnat m[static restrict 1], const float xr[static res
 
   register const VS xe = _mm512_set1_ps(-ex);
   register const VS ye = _mm512_set1_ps(-ey);
-
+  // USE_2SUM: Kahan + Graillat & al.
+#ifdef USE_2SUM
+  register const VS _zerof = _mm512_set1_ps(-0.0f);
+  register VS sXrYr = _mm512_setzero_ps();
+  register VS tXrYr = _mm512_setzero_ps();
+  register VS sXiYi = _mm512_setzero_ps();
+  register VS tXiYi = _mm512_setzero_ps();
+  register VS sXrYi = _mm512_setzero_ps();
+  register VS tXrYi = _mm512_setzero_ps();
+  register VS sXiYr = _mm512_setzero_ps();
+  register VS tXiYr = _mm512_setzero_ps();
+  register VS sXrYr_ = _mm512_setzero_ps();
+  register VS tXrYr_ = _mm512_setzero_ps();
+  register VS sXiYi_ = _mm512_setzero_ps();
+  register VS tXiYi_ = _mm512_setzero_ps();
+  register VS sXrYi_ = _mm512_setzero_ps();
+  register VS tXrYi_ = _mm512_setzero_ps();
+  register VS sXiYr_ = _mm512_setzero_ps();
+  register VS tXiYr_ = _mm512_setzero_ps();
+  register VS a, b, a_, b_, xy, xy_;
+  const int rni = (_MM_FROUND_TO_NEG_INF | _MM_FROUND_NO_EXC);
+  for (fnat i = 0u; i < *m; i += VSL) {
+    register VS xri = _mm512_load_ps(xr + i);
+    register VS yri = _mm512_load_ps(yr + i);
+    register VS yii = _mm512_load_ps(yi + i);
+    register VS xii = _mm512_load_ps(xi + i);
+    xri = _mm512_scalef_ps(xri, xe); VSP(xri);
+    yri = _mm512_scalef_ps(yri, ye); VSP(yri);
+    yii = _mm512_scalef_ps(yii, ye); VSP(yii);
+    xii = _mm512_scalef_ps(xii, xe); VSP(xii);
+    INMULSUM(xri,yri,sXrYr,tXrYr,sXrYr_,tXrYr_);
+    INMULSUM(xii,yii,sXiYi,tXiYi,sXiYi_,tXiYi_);
+    xii = VSNEG(xii); VSP(xii);
+    INMULSUM(xri,yii,sXrYi,tXrYi,sXrYi_,tXrYi_);
+    INMULSUM(xii,yri,sXiYr,tXiYr,sXiYr_,tXiYr_);
+  }
+#else /* !USE_2SUM */
   register VS pr = _mm512_setzero_ps();
   register VS pi = _mm512_setzero_ps();
-
   for (fnat i = 0u; i < *m; i += VSL) {
     register VS xri = _mm512_load_ps(xr + i);
     register VS yri = _mm512_load_ps(yr + i);
@@ -53,10 +114,29 @@ float complex cdpscl_(const fnat m[static restrict 1], const float xr[static res
     pr = _mm512_fmadd_ps(xii, yii, pr); VSP(pr);
     pi = _mm512_fnmadd_ps(xii, yri, pi); VSP(pi);
   }
+#endif /* ?USE_2SUM */
 
   const float fx = f[0u];
   const float fy = f[1u];
+#ifdef USE_2SUM
+  OUTSUM(tXrYr_,tXiYi_);
+  OUTSUM(sXrYr_,sXiYi_);
+  OUTSUM(tXrYr,tXiYi);
+  OUTSUM(sXrYr,sXiYi);
+  const float nur =
+    _mm512_reduce_add_ps(sXrYr) + (_mm512_reduce_add_ps(sXiYi) + (_mm512_reduce_add_ps(tXrYr) + (_mm512_reduce_add_ps(tXiYi) + (_mm512_reduce_add_ps(sXrYr_) + (_mm512_reduce_add_ps(sXiYi_) + (_mm512_reduce_add_ps(tXrYr_) + _mm512_reduce_add_ps(tXiYi_)))))));
+  OUTSUM(tXrYi_,tXiYr_);
+  OUTSUM(sXrYi_,sXiYr_);
+  OUTSUM(tXrYi,tXiYr);
+  OUTSUM(sXrYi,sXiYr);
+  const float nui =
+    _mm512_reduce_add_ps(sXrYi) + (_mm512_reduce_add_ps(sXiYr) + (_mm512_reduce_add_ps(tXrYi) + (_mm512_reduce_add_ps(tXiYr) + (_mm512_reduce_add_ps(sXrYi_) + (_mm512_reduce_add_ps(sXiYr_) + (_mm512_reduce_add_ps(tXrYi_) + _mm512_reduce_add_ps(tXiYr_)))))));
+#else /* !USE_2SUM */
+  const float nur = _mm512_reduce_add_ps(pr);
+  const float nui = _mm512_reduce_add_ps(pi);
+#endif /* ?USE_2SUM */
+  const float de = (fx * fy);
   alignas(16u) float complex z[2u];
-  _mm_store_ps((float*)z, _mm_div_ps(_mm_set_ps(0.0f, 1.0f, _mm512_reduce_add_ps(pi), _mm512_reduce_add_ps(pr)), _mm_set1_ps(fx * fy)));
+  _mm_store_ps((float*)z, _mm_div_ps(_mm_set_ps(0.0f, 1.0f, nui, nur), _mm_set1_ps(de)));
   return *z;
 }
--- src/zdpscl.c
+++ src/zdpscl.c
@@ -1,5 +1,31 @@
 #include "zdpscl.h"
 
+#ifdef USE_2SUM
+#include "d2sum.h"
+#include "vecdef.h"
+#ifdef INMULSUM
+#error INMULSUM already defined
+#else /* !INMULSUM */
+#define INMULSUM(x,y,s,t,s_,t_)       \
+  xy = _mm512_mul_round_pd(x, y, rni);\
+  a = s;                              \
+  xy_ = _mm512_fmsub_pd(x, y, xy);    \
+  b = _mm512_add_pd(xy, t);           \
+  TwoSum(a,b,a_,b_,s,t);              \
+  a = s_;                             \
+  b = _mm512_add_pd(xy_, t_);         \
+  TwoSum(a,b,a_,b_,s_,t_)
+#endif /* ?INMULSUM */
+#ifdef OUTSUM
+#error OUTSUM already defined
+#else /* !OUTSUM */
+#define OUTSUM(x,y)    \
+  a = x;               \
+  b = y;               \
+  TwoSum(a,b,a_,b_,x,y)
+#endif /* ?OUTSUM */
+#endif /* USE_2SUM */
+
 double complex zdpscl_(const fnat m[static restrict 1], const double xr[static restrict VDL], const double xi[static restrict VDL], const double yr[static restrict VDL], const double yi[static restrict VDL], const double e[static restrict 2], const double f[static restrict 2])
 {
 #ifndef NDEBUG
@@ -36,9 +62,45 @@ double complex zdpscl_(const fnat m[static restrict 1], const double xr[static r
   register const VD xe = _mm512_set1_pd(-ex);
   register const VD ye = _mm512_set1_pd(-ey);
 
+  // USE_2SUM: Kahan + Graillat & al.
+#ifdef USE_2SUM
+  register const VD _zero = _mm512_set1_pd(-0.0);
+  register VD sXrYr = _mm512_setzero_pd();
+  register VD tXrYr = _mm512_setzero_pd();
+  register VD sXiYi = _mm512_setzero_pd();
+  register VD tXiYi = _mm512_setzero_pd();
+  register VD sXrYi = _mm512_setzero_pd();
+  register VD tXrYi = _mm512_setzero_pd();
+  register VD sXiYr = _mm512_setzero_pd();
+  register VD tXiYr = _mm512_setzero_pd();
+  register VD sXrYr_ = _mm512_setzero_pd();
+  register VD tXrYr_ = _mm512_setzero_pd();
+  register VD sXiYi_ = _mm512_setzero_pd();
+  register VD tXiYi_ = _mm512_setzero_pd();
+  register VD sXrYi_ = _mm512_setzero_pd();
+  register VD tXrYi_ = _mm512_setzero_pd();
+  register VD sXiYr_ = _mm512_setzero_pd();
+  register VD tXiYr_ = _mm512_setzero_pd();
+  register VD a, b, a_, b_, xy, xy_;
+  const int rni = (_MM_FROUND_TO_NEG_INF | _MM_FROUND_NO_EXC);
+  for (fnat i = 0u; i < *m; i += VDL) {
+    register VD xri = _mm512_load_pd(xr + i);
+    register VD yri = _mm512_load_pd(yr + i);
+    register VD yii = _mm512_load_pd(yi + i);
+    register VD xii = _mm512_load_pd(xi + i);
+    xri = _mm512_scalef_pd(xri, xe); VDP(xri);
+    yri = _mm512_scalef_pd(yri, ye); VDP(yri);
+    yii = _mm512_scalef_pd(yii, ye); VDP(yii);
+    xii = _mm512_scalef_pd(xii, xe); VDP(xii);
+    INMULSUM(xri,yri,sXrYr,tXrYr,sXrYr_,tXrYr_);
+    INMULSUM(xii,yii,sXiYi,tXiYi,sXiYi_,tXiYi_);
+    xii = VDNEG(xii); VDP(xii);
+    INMULSUM(xri,yii,sXrYi,tXrYi,sXrYi_,tXrYi_);
+    INMULSUM(xii,yri,sXiYr,tXiYr,sXiYr_,tXiYr_);
+  }
+#else /* !USE_2SUM */
   register VD pr = _mm512_setzero_pd();
   register VD pi = _mm512_setzero_pd();
-
   for (fnat i = 0u; i < *m; i += VDL) {
     register VD xri = _mm512_load_pd(xr + i);
     register VD yri = _mm512_load_pd(yr + i);
@@ -53,10 +115,29 @@ double complex zdpscl_(const fnat m[static restrict 1], const double xr[static r
     pr = _mm512_fmadd_pd(xii, yii, pr); VDP(pr);
     pi = _mm512_fnmadd_pd(xii, yri, pi); VDP(pi);
   }
+#endif /* ?USE_2SUM */
 
   const double fx = f[0u];
   const double fy = f[1u];
+#ifdef USE_2SUM
+  OUTSUM(tXrYr_,tXiYi_);
+  OUTSUM(sXrYr_,sXiYi_);
+  OUTSUM(tXrYr,tXiYi);
+  OUTSUM(sXrYr,sXiYi);
+  const double nur =
+    _mm512_reduce_add_pd(sXrYr) + (_mm512_reduce_add_pd(sXiYi) + (_mm512_reduce_add_pd(tXrYr) + (_mm512_reduce_add_pd(tXiYi) + (_mm512_reduce_add_pd(sXrYr_) + (_mm512_reduce_add_pd(sXiYi_) + (_mm512_reduce_add_pd(tXrYr_) + _mm512_reduce_add_pd(tXiYi_)))))));
+  OUTSUM(tXrYi_,tXiYr_);
+  OUTSUM(sXrYi_,sXiYr_);
+  OUTSUM(tXrYi,tXiYr);
+  OUTSUM(sXrYi,sXiYr);
+  const double nui =
+    _mm512_reduce_add_pd(sXrYi) + (_mm512_reduce_add_pd(sXiYr) + (_mm512_reduce_add_pd(tXrYi) + (_mm512_reduce_add_pd(tXiYr) + (_mm512_reduce_add_pd(sXrYi_) + (_mm512_reduce_add_pd(sXiYr_) + (_mm512_reduce_add_pd(tXrYi_) + _mm512_reduce_add_pd(tXiYr_)))))));
+#else /* !USE_2SUM */
+  const double nur = _mm512_reduce_add_pd(pr);
+  const double nui = _mm512_reduce_add_pd(pi);
+#endif /* ?USE_2SUM */
+  const double de = (fx * fy);
   alignas(16u) double complex z;
-  _mm_store_pd((double*)&z, _mm_div_pd(_mm_set_pd(_mm512_reduce_add_pd(pi), _mm512_reduce_add_pd(pr)), _mm_set1_pd(fx * fy)));
+  _mm_store_pd((double*)&z, _mm_div_pd(_mm_set_pd(nui, nur), _mm_set1_pd(de)));
   return z;
 }
