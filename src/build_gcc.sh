1ca5e36f3e836895068de3ba75ea69c4b98696c7
Vedran Novakovic <venovako@venovako.eu>
2026-06-16 03:57:03 +0200

    further updates

--- src/build_gcc.sh
+++ src/build_gcc.sh
@@ -3,14 +3,14 @@
 if [ -z "${GNU}" ]
 then
 	cd ../../libpvn/src
-	make COMPILER=gcc NDEBUG=3 SAFE=SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 clean
-	make COMPILER=gcc NDEBUG=3 SAFE=SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j all
+	make COMPILER=gcc NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 clean
+	make COMPILER=gcc NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j all
 	cd ../../VecJac/src
 	make COMPILER=gnu clean all
 else
 	cd ../../libpvn/src
-	make COMPILER=gcc COMPILER_SUFFIX=${GNU} NDEBUG=3 SAFE=SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 clean
-	make COMPILER=gcc COMPILER_SUFFIX=${GNU} NDEBUG=3 SAFE=SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j all
+	make COMPILER=gcc COMPILER_SUFFIX=${GNU} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 clean
+	make COMPILER=gcc COMPILER_SUFFIX=${GNU} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j all
 	cd ../../VecJac/src
 	make COMPILER=gnu clean all
 fi
