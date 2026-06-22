4bfe4f21fbb374bd4055a2d84b9e1d0d67975f7f
Vedran Novakovic <venovako@venovako.eu>
2026-06-22 19:55:13 +0200

    further updates

--- src/build_gcc.sh
+++ src/build_gcc.sh
@@ -4,13 +4,13 @@ if [ -z "${GNU}" ]
 then
 	cd ../../libpvn/src
 	make COMPILER=gcc NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 clean
-	make COMPILER=gcc NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j all
-	cd ../../VecJac/src
+	make COMPILER=gcc NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j
+	cd -
 	make COMPILER=gnu clean all
 else
 	cd ../../libpvn/src
 	make COMPILER=gcc COMPILER_SUFFIX=${GNU} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 clean
-	make COMPILER=gcc COMPILER_SUFFIX=${GNU} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j all
-	cd ../../VecJac/src
+	make COMPILER=gcc COMPILER_SUFFIX=${GNU} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j
+	cd -
 	make COMPILER=gnu clean all
 fi
--- src/build_icx.sh
+++ src/build_icx.sh
@@ -4,13 +4,13 @@ if [ -z "${TGT}" ]
 then
 	cd ../../libpvn/src
 	make COMPILER=icx NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 clean
-	make COMPILER=icx NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j all
-	cd ../../VecJac/src
+	make COMPILER=icx NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j
+	cd -
 	make clean all
 else
 	cd ../../libpvn/src
-	make COMPILER=icx MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 clean
-	make COMPILER=icx MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j all
-	cd ../../VecJac/src
+	make COMPILER=icx NATIVE=haswell MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 clean
+	make COMPILER=icx NATIVE=haswell MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 GMP=/opt/gmp MPFR=/opt/mpfr SLEEF=/opt/sleef sleef=0 -j
+	cd -
 	make clean all
 fi
